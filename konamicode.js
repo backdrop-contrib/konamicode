ba0f0c4ec89e0c84c583d681647fbcc6cd51d468
Rob Loach <robloach@gmail.com>
2010-03-03 04:49:44 +0000

    #730714: Multiple Konami Code actions for Image Attack plugin.

--- konamicode.js
+++ konamicode.js
@@ -1,23 +1,108 @@
 /* $Id$ */
 
 /**
- * @file
- * Redirects the user to a page when they enter the Konami Code.
+ * Konami Code jQuery Plugin
  */
+(function($) {
+  var konamiListeners = [];
+  var progress = [];
+
+  $.extend({
+    konami: function(callback, sequence) {
+      sequence = typeof(sequence) != 'undefined' ? sequence : [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+      konamiListeners.push({'callback': callback, 'sequence': sequence});
+    }
+  });
+
+  $(document).bind("keyup", function(event) {
+    progress.push(event.keyCode);
+    $.each(konamiListeners, function(index, listener) {
+      // Only compare if it's a possible full match.
+      if (progress.length >= listener.sequence.length) {
+        // Create a target sequence that is the same length as the progress.
+        var target = progress.slice(progress.length - listener.sequence.length);
+        // Check if the result is the same.
+        var equals = true;
+        for (var i = 0; i < target.length; i++) {
+          if (target[i] != listener.sequence[i]) {
+            equals = false;
+            break;
+          }
+        }
+        if (equals) {
+          // Reset the progress and invoke the listener.
+          progress = [];
+          listener.callback();
+          return false;
+        }
+      }
+    });
+    // Keep the progress length sane.
+    if (progress.length > 40) {
+      progress = progress.slice(25);
+    }
+  });
+})(jQuery);
 
 /**
- * The Drupal behavior to redirect the user when they enter the Konami Code.
- *
- * Thank you to jQuery.com and John Resig for the Konami Code event listener.
+ * Register the Konami Code action behavior.
  */
-Drupal.behaviors.konamicode = function() {
-  if (window.addEventListener) {
-    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-    window.addEventListener("keydown", function(e){
-      kkeys.push(e.keyCode);
-      if (kkeys.toString().indexOf(konami) >= 0) {
-        window.location = Drupal.settings.konamicode;
-      }
-    }, true);
-  }
+Drupal.behaviors.konamicode = function(context) {
+  // Multiple actions can take place.
+  $.each(Drupal.settings.konamicode || {}, function(action, code) {
+    var sequence = (code == true) ? [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] : code;
+    // Register the Konami Code event.
+    $('body:not(.konamicode' + action + ')').addClass('konamicode' + action).each(function() {
+      $.konami(function() {
+        // Activate the event.
+        Drupal['konamicode_' + action]();
+      }, sequence);
+    });
+  });
 };
+
+/**
+ * The Image Attack Konami Code action.
+ */
+Drupal.konamicode_imageattack = function() {
+  // Subtract Druplicon width and height to ensure that he is only spawned
+  // inside the window area and does not cause it to scroll.
+  var width = $(window).width() - 175;
+  var height = $(window).height() - 200;
+  var druplicon = Drupal.settings.konamicodeImage || 'http://drupalcode.org/viewvc/drupal/contributions/docs/marketing/logo/druplicon.small.png?view=co';
+  var max = 500;
+  var count = 0;
+  konamiCodeSpawnImage(width, height, druplicon, max, count);
+};
+
+/**
+ * Spawn an image randomly on the screen.
+ */
+function konamiCodeSpawnImage(width, height, image, max, count) {
+  // Generate random location.
+  var x = Math.floor(Math.random() * width);
+  var y = Math.floor(Math.random() * height);
+
+  // Append Druplicon image tag to HTML body.
+  $('body').append('<img src="' + image + '" style="position: absolute; z-index: 1000; left: ' + x + 'px; top: ' + y + 'px;"/>');
+  count++;
+  
+  // Queue another Druplicon.
+  if (count < max) {
+    setTimeout('konamiCodeSpawnImage(' + width + ', ' + height + ', "' + image + '", ' + max + ', ' + count + ')', 10);
+  }
+}
+
+/**
+ * The Redirect Konami Code action.
+ */
+Drupal.konamicode_redirect = function() {
+  window.location = Drupal.settings.konamicodeDestination || 'http://bacolicio.us/' + window.location;
+};
+
+/**
+ * The Alert Konami Code action.
+ */
+Drupal.konamicode_alert = function() {
+  alert(Drupal.settings.konamicodeAlert || Drupal.t('Konami Code is geek!'));
+}
