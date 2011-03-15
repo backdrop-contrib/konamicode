a72dd4579804a7b7c396af0e9bc5c205acfb0811
Rob Loach <robloach@gmail.com>
2011-03-15 06:45:46 -0700

    Issue #1088146 by decibel.places, Rob Loach: New Drupalicon image URL.

--- konamicode.js
+++ konamicode.js
@@ -84,7 +84,7 @@ Drupal.konamicode_imageattack = function() {
   // inside the window area and does not cause it to scroll.
   var width = jQuery(document).width() - 175;
   var height = jQuery(document).height() - 200;
-  Drupal.konamicode_imageattackimages = Drupal.settings.konamicodeImages || ['http://drupalcode.org/viewvc/drupal/contributions/docs/marketing/logo/druplicon.small.png?view=co'];
+  Drupal.konamicode_imageattackimages = Drupal.settings.konamicodeImages || ['http://drupal.org/files/druplicon.small_.png'];
   // Select a random image.
   var max = Drupal.settings.konamicodeImagesMax || 500;
   var count = 0;
