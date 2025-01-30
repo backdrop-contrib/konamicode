b144283b74e89772477f0c33e1a0073f4075c3bf
keiserjb <keiserjb@yahoo.com>
2025-01-30 14:37:11 -0500

    add cheetos font and airplane

--- konamicode.js
+++ konamicode.js
@@ -172,6 +172,73 @@ Backdrop.konamicode_fliptext = function() {
 };
 
 /**
+ * The CheetosFont Konami Code action.
+ */
+Backdrop.konamicode_cheetosfont = function () {
+  console.log("Konami Code triggered - toggling CheetosFont!");
+
+  if ($('html').hasClass('cheetosfont-active')) {
+    // Remove the font
+    $('html').removeClass('cheetosfont-active');
+    console.log("CheetosFont removed.");
+
+    // Force reflow to apply changes
+    $('body, *').css('font-family', '');
+  } else {
+    // Apply the font
+    $('html').addClass('cheetosfont-active');
+    console.log("CheetosFont applied!");
+
+    // Force reflow and apply the font dynamically
+    $('body, *').css('font-family', 'OtherHand, sans-serif');
+  }
+};
+
+/**
+ * The Airplane Konami Code action.
+ */
+
+Backdrop.konamicode_airplane = function () {
+  console.log("Konami Code triggered - Airplane looping and landing!");
+
+  // Check if airplane is already flying
+  if ($('#konami-airplane-wrapper').length) {
+    console.log("Airplane already in motion!");
+    return;
+  }
+
+  // Define the new SVG airplane
+  let airplaneSVG = `
+  <div id="konami-airplane-wrapper">
+    <svg id="konami-airplane" width="1000" height="1000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path fill="#000" d="M76.183 264.465l11.224 2.393-4.357 11.62a12.27 12.27 0 1 1-10.353-4.73zM378.67 321.61a21.078 21.078 0 1 0 11.968-5.17l-5.81-13.55h-14.257zm69.208-75.53c0 6.578-.221 12.934-.628 18.686h10.18v-33.93h-9.97c.232 4.845.406 9.981.406 15.28zm-22.903 40.438h-80.7c-115.408 0-213.503-28.504-286.336-42.459C26.415 238.018 21 226.526 21 213.151c0-17.894 9.633-59.656 39.24-59.656 24.576 0 53.08 61.364 62.422 61.364 3.986 0 170.033-9.144 170.033-9.144 0 8.354 13.851 12.526 27.714 12.526 13.862 0 27.887-4.218 27.887-12.62v-22.519s23.868 12.782 30.55 22.566h46.107c8.796 0 8.796 80.886.011 80.886zM408.8 247.802c0-3.998-3.66-7.251-8.134-7.251H248.168c-4.508 0-8.134 3.242-8.134 7.25 0 4.01 3.66 7.251 8.134 7.251h152.453c4.508.035 8.168-3.218 8.168-7.215zm73.984-57.007a8.134 8.134 0 0 0-8.134 8.134v94.283a8.175 8.175 0 0 0 16.349 0v-94.214a8.134 8.134 0 0 0-8.227-8.169z"/>
+    </svg>
+  </div>
+  `;
+
+  // Append the airplane to the body
+  $('body').append(airplaneSVG);
+
+  // Start looping motion
+  $('#konami-airplane-wrapper').addClass('fly-loop');
+
+  // After looping, land and then exit off-screen
+  setTimeout(() => {
+    $('#konami-airplane-wrapper').removeClass('fly-loop').addClass('land');
+  }, 8000); // Match landing timing
+
+  setTimeout(() => {
+    $('#konami-airplane-wrapper').addClass('exit');
+  }, 10000); // After landing, move off-screen
+
+  // Remove the airplane entirely after the exit animation
+  setTimeout(() => {
+    $('#konami-airplane-wrapper').remove();
+    console.log("Airplane has landed and taxied away!");
+  }, 12000);
+};
+
+/**
  * The Cornify Konami Code action.
  */
 Backdrop.konamicode_cornify = function() {
